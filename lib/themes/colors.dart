5d568fbbd7e29400f7e2921b4fe158d580d224e4
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-11-11 21:20:49 +0700

    feat: add color constants

---
+++ lib/themes/colors.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+const Color blackText = Color(0xFF1C1E1D);
+const Color blackSoftText = Color(0xFF494B4A);
+const Color white = Color(0xFFFFFFFF);
+const Color whiteL2 = Color(0xFFFAFAFA);
+const Color green = Color(0xFF00880C);
+const Color darkGreen = Color(0xFF077310);
+const Color red = Color(0xFFE9001E);
+const Color blue = Color(0xFF0081A0);
+const Color softGrey = Color(0xFFE8E8E8);
+const Color grey = Color(0xFFBBBBBB);
+const Color softGreen = Color(0xFFE9FFEA);
