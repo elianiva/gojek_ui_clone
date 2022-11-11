5e811d27a9947f984160073e963c1d0d11671035
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-11-11 21:24:59 +0700

    feat: add text style constants

---
+++ lib/themes/text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:gojek_clone/themes/colors.dart' as colors;
+import 'package:gojek_clone/themes/font_sizes.dart' as font_sizes;
+
+const TextStyle textButtonBlack = TextStyle(
+  color: colors.blackText,
+  fontSize: font_sizes.medium2,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle textButtonWhite = TextStyle(
+  color: colors.white,
+  fontSize: font_sizes.medium2,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle textTitleBlack = TextStyle(
+  color: colors.blackText,
+  fontSize: font_sizes.large2,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle textParagraphBlack = TextStyle(
+  color: colors.blackSoftText,
+  fontSize: font_sizes.medium2,
+);
