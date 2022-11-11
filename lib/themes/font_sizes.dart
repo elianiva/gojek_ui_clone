39f0c88acd5301d6f82ba538c3983a6da92ba1fc
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-11-11 21:24:49 +0700

    feat: add font size constants

---
+++ lib/themes/font_sizes.dart
@@ -0,0 +1,8 @@
+const double small1 = 8;
+const double small2 = 10;
+const double small3 = 12;
+const double medium1 = 14;
+const double medium2 = 16;
+const double large1 = 18;
+const double large2 = 20;
+const double large3 = 22;
