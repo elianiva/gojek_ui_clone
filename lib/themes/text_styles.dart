115aea74665b1381c8eace42b12aaa57ec0fee7a
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-11-12 07:09:18 +0700

    refactor: adjust bottom nav stylings

--- lib/themes/text_styles.dart
+++ lib/themes/text_styles.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:gojek_clone/themes/colors.dart';
 import 'package:gojek_clone/themes/font_sizes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GojekTextStyles {
-  static const TextStyle textBold = TextStyle(
+  static TextStyle textBold = GoogleFonts.roboto(
     fontSize: GojekFontSize.medium1,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle textButtonBlack = TextStyle(
+  static TextStyle textButtonBlack = GoogleFonts.roboto(
     color: GojekColor.blackText,
     fontSize: GojekFontSize.medium2,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle textButtonWhite = TextStyle(
+  static TextStyle textButtonWhite = GoogleFonts.roboto(
     color: GojekColor.white,
     fontSize: GojekFontSize.medium2,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle textTitleBlack = TextStyle(
+  static TextStyle textTitleBlack = GoogleFonts.roboto(
     color: GojekColor.blackText,
     fontSize: GojekFontSize.large2,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle textParagraphBlack = TextStyle(
+  static TextStyle textParagraphBlack = GoogleFonts.roboto(
     color: GojekColor.blackSoftText,
     fontSize: GojekFontSize.medium2,
   );
--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -10,11 +10,31 @@ class FloatingBottomNav extends StatefulWidget {
 }
 
 class FloatingBottomNavState extends State<FloatingBottomNav> {
-  final List<Widget> menu = [
-    LabeledButtonIcon(label: "GoRide", iconPath: "assets/icons/ic_indoride.png"),
-    LabeledButtonIcon(label: "GoCar", iconPath: "assets/icons/ic_indocar.png"),
-    LabeledButtonIcon(label: "GoFood", iconPath: "assets/icons/ic_indofood.png"),
-    LabeledButtonIcon(label: "GoSend", iconPath: "assets/icons/ic_indosend.png"),
+  late final List<Widget> menu = [
+    LabeledButtonIcon(
+      label: "GoRide",
+      iconPath: "assets/icons/ic_indoride.png",
+      iconHeight: 42,
+      iconWidth: 42,
+    ),
+    LabeledButtonIcon(
+      label: "GoCar",
+      iconPath: "assets/icons/ic_indocar.png",
+      iconHeight: 42,
+      iconWidth: 42,
+    ),
+    LabeledButtonIcon(
+      label: "GoFood",
+      iconPath: "assets/icons/ic_indofood.png",
+      iconHeight: 42,
+      iconWidth: 42,
+    ),
+    LabeledButtonIcon(
+      label: "GoSend",
+      iconPath: "assets/icons/ic_indosend.png",
+      iconHeight: 42,
+      iconWidth: 42,
+    ),
   ];
 
   @override
@@ -23,14 +43,28 @@ class FloatingBottomNavState extends State<FloatingBottomNav> {
       padding: const EdgeInsets.all(28.0),
       child: Card(
         color: GojekColor.white,
-        elevation: 8.0,
+        elevation: 10.0,
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(64.0)),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 28.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: menu,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 40,
+                height: 6,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: GojekColor.grey,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: menu,
+              ),
+            ],
           ),
         ),
       ),
